6784fbbcc5e0a4c2ba279d0d2b2a2bd52b7bd0ea
patricio <pgarciafarrell@frba.utn.edu.ar>
2022-07-27 21:29:31 -0300

    Update vacunacion.py

--- covidapp/vacunacion.py
+++ covidapp/vacunacion.py
@@ -9,11 +9,11 @@ import numpy as np
 
 param_dic = {
     #"host"      : "localhost",
-    "host"      : "cabacovid.cs4nf40vsq8e.us-east-1.rds.amazonaws.com",
+    "host"      : "-",
     "port"      : "5432",
     "database"  : "cabacovid",
-    "user"      : "covid_app",
-    "password"  : "covid_app"
+    "user"      : "-",
+    "password"  : "-"
 }
 
 def execute_query(conn, query):
